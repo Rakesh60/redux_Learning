e7d4c7672063d218f8ea876a982849143c79adc1
Rakesh60 <rakeshibm909@gmail.com>
2024-06-26 16:54:00 +0530

    folder structure

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -8,10 +8,12 @@ function Folder({ files }) {
   return (
     <div className='folder'>
       <div onClick={()=>{setExpand(!expand)}}>
+        
         {files.isFolder?(<button  className={expand?'expand':''}>
           {">"}
         </button>):null}
-        {files.name}</div>
+        {files.name}
+        </div>
         {
           files.isFolder && expand && files.children.map((exp,index)=> <div key={index}>
           <Folder files={exp}/>
